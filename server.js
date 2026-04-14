9d38b03eae5fd42f5ee0a1f885e66819cdaaf237
JuGuan0716 <kensaw0716@gmail.com>
2026-04-14 14:33:38 +0800

    fix railway bind

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+// MUST use Railway port
+const PORT = process.env.PORT;
 
 // serve static files
 app.use(express.static(path.join(__dirname)));
 
-// default route
+// homepage route
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
+// IMPORTANT: bind to 0.0.0.0
+app.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port " + PORT);
 });
