81f6eeb1e2a63188805ef72ce75887bb62c07b7c
JuGuan0716 <kensaw0716@gmail.com>
2026-04-14 14:35:20 +0800

    fix railway routing

--- server.js
+++ server.js
@@ -3,18 +3,21 @@ const path = require("path");
 
 const app = express();
 
-// MUST use Railway port
 const PORT = process.env.PORT;
 
-// serve static files
-app.use(express.static(path.join(__dirname)));
+// IMPORTANT: serve static FIRST
+app.use(express.static(__dirname));
 
-// homepage route
+// FORCE root to index.html
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// IMPORTANT: bind to 0.0.0.0
+// CATCH ALL (important fix)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port " + PORT);
 });
